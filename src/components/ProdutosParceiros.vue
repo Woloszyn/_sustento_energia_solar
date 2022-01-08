97299b2a5b747595671df6c404c6550c8c5c6abd
luanahoegen <luanahoegen123@gmail.com>
2022-01-08 15:20:38 -0300

    Starting component Produtos Parceiros

--- src/components/ProdutosParceiros.vue
+++ src/components/ProdutosParceiros.vue
@@ -1,6 +1,17 @@
 <template>
     <div id="produtos_e_parceiros">
-        
+        <div class="col-12">
+            <div class="row produtos_parceiros">
+                <div class="col-7">
+                    <div class="module-tittle">PRODUTOS E PARCEIROS</div>
+                </div>
+                <div class="col-5">
+                    <div class="textoProdutosParceiros">
+                        Conheça nossos parceiros e produtos que trabalhamos.
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,5 +22,14 @@ export default {
 </script>
 
 <style>
-
+    .produtos_parceiros{
+        background-image: url('../assets/orcamento/title-orcamento-online.png') ;
+        background-size: 100%;
+        background-repeat: no-repeat;
+    }
+    .textoProdutosParceiros {
+        font-style: italic;
+        color: #212529;
+        font-size: 18px;
+    }
 </style>
