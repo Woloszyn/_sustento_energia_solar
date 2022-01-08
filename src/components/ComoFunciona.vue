4df5851c35c80b666902b5925d197e99d2e7f368
luanahoegen <luanahoegen123@gmail.com>
2022-01-08 14:52:22 -0300

    Starting component Como Funciona

--- src/components/ComoFunciona.vue
+++ src/components/ComoFunciona.vue
@@ -1,6 +1,34 @@
 <template>
     <div id="como_funciona">
-
+        <div class="col-12">
+            <div class="row tituloComoFunciona">
+                <div class="col-7">
+                    <div class="module-tittle">O SISTEMA FUNCIONA ASSIM</div>
+                </div>
+            </div>
+        </div>
+        <div class="col-11">
+            <div class="row">
+                <div class="col-6">
+                    <img src="../assets/orcamento/funcionamento-min.jpg" alt="">
+                </div>
+                <div class="col-6">
+                    <div class="paragrafos">
+                        <p>A energia solar funciona por meio da irradiação solar, ou seja, pela luminosidade solar e não por meio do seu calor. 
+                            Basicamente, quanto mais ensolarado e claro o dia, mais energia é gerada.
+                        </p>
+                        <p>
+                            Os módulos fotovoltaicos, popularmente chamados de painéis,
+                            recebem a irradiação solar e transformam a luz do sol em energia elétrica.
+                        </p>
+                        <p>
+                            A energia gerada passa por um inversor, ou microinversor, que a iguala ao tipo de corrente que recebemos da rede pública. Deste modo, nos beneficiamos da energia solar e compensamos da conta de energia a quantidade produzida pelos módulos. 
+                            Assim, é possível até compartilhá-la entre contas de energias cadastradas no mesmo CPF ou CNPJ.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,5 +39,17 @@ export default {
 </script>
 
 <style>
-
+    .tituloComoFunciona {
+        background-image: url('../assets/orcamento/title-orcamento-online.png');
+        background-size: 100%;
+        background-repeat: no-repeat;
+        height: 90px;
+    }
+    .paragrafos {
+        color: #000;
+        font-style: italic;
+        font-weight: 500;
+        font-size: 16px;
+        text-align: left;
+    }
 </style>
---
+++ src/components/ProdutosParceiros.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="produtos_e_parceiros">
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
